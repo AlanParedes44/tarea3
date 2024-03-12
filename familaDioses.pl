96e520f3e8538da96c542563e80eedb3d774090b
AlanParedes44 <161607785+AlanParedes44@users.noreply.github.com>
2024-03-11 22:35:36 -0400

    familaDioses.pl

--- familaDioses.pl
+++ familaDioses.pl
@@ -20,25 +20,96 @@ esHombre(zeus).
 esHombre(hefesto).
 
 
+%hombre
+esHombre(cronos).
+esHombre(ares).
+esHombre(poseidon).
+esHombre(hefesto).
+esHombre(zeus).
+esHombre(baco).
+esHombre(hermes).
+%mujer
 esMujer(hera).
-esMujer(atena).
 esMujer(afrodita).
-esMujer(nera).
 esMujer(hipolita).
+esMujer(nera).
+esMujer(atena).
+%decendienteDirecto
+esDecendienteDirecto(hera,cronos).
+esDecendienteDirecto(hefesto,ares).
+esDecendienteDirecto(hefesto,hera).
+esDecendienteDirecto(afrodita,ares).
+esDecendienteDirecto(afrodita,hera).
+esDecendienteDirecto(zeus,afrodita).
+esDecendienteDirecto(zeus,poseidon).
+esDecendienteDirecto(nera,zeus).
+esDecendienteDirecto(nera,hipolita).
+esDecendienteDirecto(baco,afrodita).
+esDecendienteDirecto(baco,hefesto).
+esDecendienteDirecto(baco,afrodita).
+esDecendienteDirecto(atena,hefesto).
+esDecendienteDirecto(atena,afrodita).
+esDecendienteDirecto(hermes,atena).
 
-esDescendienteDirecto(hera,cronos).
-esDescendienteDirecto(afrodita,ares).
-esDescendienteDirecto(afrodita,hera).
-esDescendienteDirecto(hefesto,ares).
-esDescendienteDirecto(hefesto,hera).
-esDescendienteDirecto(zeus,poseidon).
-esDescendienteDirecto(zeus,afrodita).
-esDescendienteDirecto(baco,afrodita).
-esDescendienteDirecto(baco,hefesto).
-esDescendienteDirecto(atena,hefesto).
-esDescendienteDirecto(atena,afrodita).
-esDescendienteDirecto(nera,hipolita).
-esDescendienteDirecto(nera,zeus).
-esDescendienteDirecto(hermes,atena).
+%reglas
+%padre
+esPadre(cronos,hera).
+esPadre(ares,afrodita).
+esPadre(ares,hefesto).
+esPadre(poseidon,zeus).
+esPadre(hefesto,atena).
+esPadre(hefesto,baco).
+
+%madre
+esMadre(hera,afrodita).
+esMadre(hera,hefesto).
+esMadre(afrodita,zeus).
+esMadre(afrodita,baco).
+esMadre(afrodita,atena).
+esMadre(atena,hermes).
+
+%hijo
+esHijo(hefesto,hera).
+esHijo(hefesto,ares).
+esHijo(zeus,afrodita).
+esHijo(zeus,poseidon).
+esHijo(baco,afrodita).
+esHijo(baco,hefesto).
+esHijo(hermes,atena).
+
+%hija
+esHija(hera,cronos).
+esHija(afrodita,hera).
+esHija(afrodita,ares).
+esHija(atena,hefesto).
+esHija(nera,zeus).
+esHija(nera,hipolita).
+
+%hermano
+esHermano(hefesto,afrodita).
+esHermano(baco, atena).
+esHermano(zeus,baco).
+esHermano(baco,zeus).
+
+%hermana
+esHermana(afrodita,hefesto).
+esHermana(atena,baco).
+esHermana(atena,zeus).
+
+%abuelo
+esAbuelo(cronos,hefesto).
+esAbuelo(cronos,afrodita).
+esAbuelo(ares,atena).
+esAbuelo(ares,baco).
+esAbuelo(ares,zeus).
+esAbuelo(poseidon,nera).
+esAbuelo(hefesto,hermes).
+
+%abuela
+esAbuela(hera, atena).
+esAbuela(hera, baco).
+esAbuela(hera, zeus).
+esAbuela(afrodita, zeus).
+esAbuela(afrodita, hermes).
 
 
