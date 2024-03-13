30da22212c13191ae624795ce947e3ff42ff2f4c
unknown <lpze.alanpablo.paredes.pa@unifranz.edu.bo>
2024-03-12 21:31:05 -0400

    trabajo listas mejorado

--- lista.pl
+++ lista.pl
@@ -2,6 +2,5 @@
 % enLista(juan, [juan,maria,pedro]).
 
 
-  enLista(Valor, [Cabeza | Cola]):Valor==Cabeza.
-
+  enLista(Valor, [Cabeza | Cola]):-Valor==Cabeza.
   enLista(Valor, [_ | Cola]):-enLista==(Valor,Cola).
